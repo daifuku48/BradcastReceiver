185627d751aae1ff4aed48c15969cd023728c0a1
danil <natalya.haritonov.72@gmail.com>
2023-04-03 21:41:48 +0300

    commit

--- app/src/main/java/com/example/prohizer/MainActivity.kt
+++ app/src/main/java/com/example/prohizer/MainActivity.kt
@@ -1,40 +1,60 @@
 package com.example.prohizer
 
+import android.Manifest
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.telephony.TelephonyManager
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.prohizer.databinding.ActivityMainBinding
-import android.Manifest
 
 class MainActivity : AppCompatActivity() {
     private val PERMISSIONS_REQUEST_READ_PHONE_STATE = 1
-
+    private val PERMISSIONS_BIND_STATE = 2
     var binding : ActivityMainBinding? = null
-
+    val REQUEST_CODE_SCREENING_PERMISSION = 123
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-        val intent = Intent(this, CallTrackingService::class.java)
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(TelephonyManager.ACTION_PHONE_STATE_CHANGED)
+        val myCallReceiver = CallReceiver()
+        registerReceiver(myCallReceiver, intentFilter)
         Log.d("Service", "Service is working")
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_PHONE_STATE), PERMISSIONS_REQUEST_READ_PHONE_STATE)
         }
-        startService(intent)
+
+
+// Проверяем, есть ли у приложения разрешение на использование Call Screening Service
+        val hasScreeningPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.BIND_SCREENING_SERVICE
+        ) == PackageManager.PERMISSION_GRANTED
+
+// Если нет, то запрашиваем у пользователя разрешение
+        if (!hasScreeningPermission) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.BIND_SCREENING_SERVICE),
+                REQUEST_CODE_SCREENING_PERMISSION
+            )
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
-            PERMISSIONS_REQUEST_READ_PHONE_STATE -> {
+            REQUEST_CODE_SCREENING_PERMISSION -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
+                    // Разрешение получено, можно использовать Call Screening Service
                 } else {
-                    onDestroy()
+                    // Разрешение не получено, нельзя использовать Call Screening Service
                 }
                 return
             }
--- app/src/main/java/com/example/prohizer/MyCallScreeningService.kt
+++ app/src/main/java/com/example/prohizer/MyCallScreeningService.kt
@@ -3,13 +3,19 @@ package com.example.prohizer
 import android.content.Intent
 import android.telecom.Call
 import android.telecom.CallScreeningService
+import android.util.Log
 
 class MyCallScreeningService : CallScreeningService() {
+
     override fun onScreenCall(callDetails: Call.Details) {
         val incomingNumber = callDetails.handle.schemeSpecificPart
-        val intent = Intent("com.example.prohizer")
-        intent.putExtra("incoming_number", incomingNumber)
-        sendBroadcast(intent)
-        respondToCall(callDetails, CallResponse.Builder().build())
+        Log.d("num", incomingNumber)
+
+        val intent = Intent(applicationContext, MainActivity::class.java).apply {
+            putExtra("numberPhone", incomingNumber)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+
+        applicationContext.startActivity(intent)
     }
 }
